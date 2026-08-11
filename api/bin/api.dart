4899b8614968d00c6761f5777b9f41d26554200a
lucasrennok <lucasrenn@hotmail.com>
2026-08-11 14:03:25 -0300

    feat: add shelf

--- api/bin/api.dart
+++ api/bin/api.dart
@@ -1,5 +1,19 @@
 import 'package:api/api.dart' as api;
 
-void main(List<String> arguments) {
-  print('Hello world: ${api.calculate()}!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+void main() async {
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+
+  var server = await shelf_io.serve(handler, 'localhost', 8080);
+
+  // Enable content compression
+  server.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
