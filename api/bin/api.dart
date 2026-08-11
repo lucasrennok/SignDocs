827682dcd6bfb063e781c15ab5338592f16221a4
lucasrennok <lucasrenn@hotmail.com>
2026-08-11 14:39:40 -0300

    feat: added routes

--- api/bin/api.dart
+++ api/bin/api.dart
@@ -1,19 +1,32 @@
 import 'package:api/api.dart' as api;
 
+import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart';
+
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler);
+
+Response _rootHandler(Request req) => Response.ok('Ok, working.\n');
+Response _echoHandler(Request req) {
+  final message = req.params['message'];
+  return Response.ok('$message\n');
+}
+
 
 void main() async {
-  var handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+  final ip = InternetAddress.anyIPv4 ?? '0.0.0.0';
 
-  var server = await shelf_io.serve(handler, 'localhost', 8080);
+  final handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await shelf_io.serve(handler, ip, port);
 
   // Enable content compression
   server.autoCompress = true;
 
   print('Serving at http://${server.address.host}:${server.port}');
 }
-
-Response _echoRequest(Request request) =>
-    Response.ok('Request for "${request.url}"');
